aa23276f75a81cead8951234aa42ed84dd132fc9
Pavel Evstigneev <pavel.evst@gmail.com>
2020-12-12 00:21:03 +0800

    Add logging to RenameTopicHandler

--- docker-src/mirrormaker-rename-topic-handler/src/main/java/com/slavirok/RenameTopicHandler.java
+++ docker-src/mirrormaker-rename-topic-handler/src/main/java/com/slavirok/RenameTopicHandler.java
@@ -8,26 +8,39 @@ import kafka.tools.MirrorMaker;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class RenameTopicHandler implements MirrorMaker.MirrorMakerMessageHandler {
-  private final HashMap<String, String> topicMap = new HashMap<String, String>();
-  private long NO_TIMESTAMP = -1L;
+    private final HashMap<String, String> topicMap = new HashMap<String, String>();
+    private long NO_TIMESTAMP = -1L;
 
-  public RenameTopicHandler(String topics) {
-    if(topics != null && !topics.isEmpty()){
-      for (String topicAssignment : topics.split(";")) {
-        String[] topicsArray = topicAssignment.split(",");
-        if (topicsArray.length == 2) {
-          topicMap.put(topicsArray[0], topicsArray[1]);
+    public RenameTopicHandler(String topics) {
+        System.out.println("RenameTopicHandler init");
+        if (topics != null && !topics.isEmpty()){
+            for (String topicAssignment : topics.split(";")) {
+                String[] topicsArray = topicAssignment.split(",");
+                if (topicsArray.length == 2) {
+                    System.out.println(String.format("RenameTopicHandler MAP %s -> %s", topicsArray[0], topicsArray[1]));
+                    topicMap.put(topicsArray[0], topicsArray[1]);
+                } else {
+                    System.out.println(String.format("RenameTopicHandler INVALID FORMAT %s", topicAssignment));
+                }
+            }
+        } else {
+            System.out.println("RenameTopicHandler ARGUMENT MISSING");
         }
-      }
     }
-  }
 
-  public List<ProducerRecord<byte[], byte[]>> handle(BaseConsumerRecord record) {
-      Long timestamp = record.timestamp() == NO_TIMESTAMP ? null : record.timestamp();
-      return Collections.singletonList(new ProducerRecord<byte[], byte[]>(getTopicName(record.topic()), null, timestamp, record.key(), record.value(), record.headers()));
-  }
+    public List<ProducerRecord<byte[], byte[]>> handle(BaseConsumerRecord record) {
+        Long timestamp = record.timestamp() == NO_TIMESTAMP ? null : record.timestamp();
+        return Collections.singletonList(new ProducerRecord<byte[], byte[]>(
+            getTopicName(record.topic()),
+            null,
+            timestamp,
+            record.key(),
+            record.value(),
+            record.headers()
+        ));
+    }
 
-  private String getTopicName(String currentTopicName){
-    return topicMap.containsKey(currentTopicName) ? topicMap.get(currentTopicName) : currentTopicName;
-  }
+    private String getTopicName(String currentTopicName){
+        return topicMap.containsKey(currentTopicName) ? topicMap.get(currentTopicName) : currentTopicName;
+    }
 }
